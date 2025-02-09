f81e68e2c980b159bdec9c79d59aa7c49dde0c8c
AugieAud <auds@missionreadyhq.com>
2025-02-10 10:56:09 +1300

    mock project

---
+++ types/generated/components.d.ts
@@ -0,0 +1,141 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksCta extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_ctas';
+  info: {
+    displayName: 'Cta';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    form: Schema.Attribute.Component<'elements.form', false>;
+    heading: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksHero extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    link: Schema.Attribute.Component<'elements.button-link', false>;
+    text: Schema.Attribute.Text;
+  };
+}
+
+export interface BlocksPricing extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_pricings';
+  info: {
+    displayName: 'Pricing';
+  };
+  attributes: {
+    description: Schema.Attribute.String;
+    name: Schema.Attribute.String;
+    plan: Schema.Attribute.Component<'elements.pricing-card', true>;
+  };
+}
+
+export interface BlocksRow extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_rows';
+  info: {
+    displayName: 'Row';
+  };
+  attributes: {
+    card: Schema.Attribute.Component<'elements.card', true>;
+  };
+}
+
+export interface ElementsButtonLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
+export interface ElementsCard extends Struct.ComponentSchema {
+  collectionName: 'components_elements_cards';
+  info: {
+    displayName: 'Card';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    Image: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface ElementsForm extends Struct.ComponentSchema {
+  collectionName: 'components_elements_forms';
+  info: {
+    displayName: 'Form';
+  };
+  attributes: {
+    button: Schema.Attribute.Component<'elements.button-link', false>;
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    input: Schema.Attribute.Component<'elements.input', true>;
+  };
+}
+
+export interface ElementsInput extends Struct.ComponentSchema {
+  collectionName: 'components_elements_inputs';
+  info: {
+    displayName: 'input';
+  };
+  attributes: {
+    inputType: Schema.Attribute.String;
+    label: Schema.Attribute.String;
+    placeholder: Schema.Attribute.String;
+  };
+}
+
+export interface ElementsPricingCard extends Struct.ComponentSchema {
+  collectionName: 'components_elements_pricing_cards';
+  info: {
+    displayName: 'Pricing card';
+  };
+  attributes: {
+    isFeatured: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.Component<'elements.button-link', false>;
+    planPrice: Schema.Attribute.String;
+    planType: Schema.Attribute.String;
+    services: Schema.Attribute.Relation<'oneToMany', 'api::service.service'>;
+  };
+}
+
+export interface SeoMetaData extends Struct.ComponentSchema {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text;
+    metaImage: Schema.Attribute.Media<'images'>;
+    metaTitle: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.cta': BlocksCta;
+      'blocks.hero': BlocksHero;
+      'blocks.pricing': BlocksPricing;
+      'blocks.row': BlocksRow;
+      'elements.button-link': ElementsButtonLink;
+      'elements.card': ElementsCard;
+      'elements.form': ElementsForm;
+      'elements.input': ElementsInput;
+      'elements.pricing-card': ElementsPricingCard;
+      'seo.meta-data': SeoMetaData;
+    }
+  }
+}
